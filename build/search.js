0388e89627cbcd14fc386d42105ce8f7f71982cc
Lorenz Schmid <schmid.lorenz@gmail.com>
2016-04-19 15:55:05 +0200

    removed entry container dependency

--- build/search.js
+++ build/search.js
@@ -11927,28 +11927,27 @@ var lunr=function(t){var e=new lunr.Index;return e.pipeline.add(lunr.stopWordFil
       }, 100);
     };
   };
-  
+
   // parse a date in yyyy-mm-dd format
   var parseDate = function(input) {
     var parts = input.match(/(\d+)/g);
     return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
   };
-  
+
   var LunrSearch = (function() {
     function LunrSearch(elem, options) {
-      this.$elem = elem;      
+      this.$elem = elem;
       this.$results = $(options.results);
-      this.$entries = $(options.entries, this.$results);
       this.indexDataUrl = options.indexUrl;
       this.template = this.compileTemplate($(options.template));
       this.emptyMsg = options.emptyMsg;
 
       this.initialize();
     }
-        
+
     LunrSearch.prototype.initialize = function() {
       var self = this;
-      
+
       this.loadIndexData(function(data) {
         self.entries = $.map(data.docs, self.createEntry);
         self.index = lunr.Index.load(data.index);
@@ -11956,16 +11955,16 @@ var lunr=function(t){var e=new lunr.Index;return e.pipeline.add(lunr.stopWordFil
         self.bindKeypress();
       });
     };
-    
+
     // compile search results template
-    LunrSearch.prototype.compileTemplate = function($template) {      
+    LunrSearch.prototype.compileTemplate = function($template) {
       var template = $template.text();
       Mustache.parse(template);
       return function (view, partials) {
         return Mustache.render(template, view, partials);
       };
     };
-        
+
     // load the search index data
     LunrSearch.prototype.loadIndexData = function(callback) {
       $.getJSON(this.indexDataUrl, callback);
@@ -11975,7 +11974,7 @@ var lunr=function(t){var e=new lunr.Index;return e.pipeline.add(lunr.stopWordFil
       var entry = $.extend({
         id: index + 1
       }, raw);
-      
+
       // include pub date for posts
       if (raw.date) {
         $.extend(entry, {
@@ -11990,10 +11989,10 @@ var lunr=function(t){var e=new lunr.Index;return e.pipeline.add(lunr.stopWordFil
           }
         });
       }
-      
+
       return entry;
     };
-    
+
     LunrSearch.prototype.bindKeypress = function() {
       var self = this;
       var oldValue = this.$elem.val();
@@ -12007,37 +12006,35 @@ var lunr=function(t){var e=new lunr.Index;return e.pipeline.add(lunr.stopWordFil
         oldValue = newValue;
       }));
     };
-    
+
     LunrSearch.prototype.search = function(query) {
       var entries = this.entries;
-      
+
       if (query.length < 3) {
-        this.$results.hide();
-        this.$entries.empty();
+        this.$results.hide().empty();
       } else {
         var results = $.map(this.index.search(query), function(result) {
           return $.grep(entries, function(entry) { return entry.id === parseInt(result.ref, 10); })[0];
         });
-        
+
         this.displayResults(results);
       }
     };
-    
+
     LunrSearch.prototype.displayResults = function(entries) {
-      var $entries = this.$entries,
-          $results = this.$results;
-        
-      $entries.empty();
-      
+      var $results = this.$results;
+
+      $results.empty();
+
       if (entries.length === 0) {
-        $entries.append('<p>'+ this.emptyMsg +'</p>');
+        $results.append('<p>'+ this.emptyMsg +'</p>');
       } else {
-        $entries.append(this.template({entries: entries}));
+        $results.append(this.template({entries: entries}));
       }
-      
+
       $results.show();
     };
-    
+
     // Populate the search input with 'q' querystring parameter if set
     LunrSearch.prototype.populateSearchFromQuery = function() {
       var uri = new URI(window.location.search.toString());
@@ -12048,24 +12045,23 @@ var lunr=function(t){var e=new lunr.Index;return e.pipeline.add(lunr.stopWordFil
         this.search(queryString.q.toString());
       }
     };
-    
+
     return LunrSearch;
   })();
 
   $.fn.lunrSearch = function(options) {
     // apply default options
-    options = $.extend({}, $.fn.lunrSearch.defaults, options);      
+    options = $.extend({}, $.fn.lunrSearch.defaults, options);
 
     // create search object
     new LunrSearch(this, options);
-    
+
     return this;
   };
-  
+
   $.fn.lunrSearch.defaults = {
     indexUrl  : '/js/index.json',   // Url for the .json file containing search index data
     results   : '#search-results',  // selector for containing search results element
-    entries   : '.entries',         // selector for search entries containing element (contained within results above)
     template  : '#search-results-template',  // selector for Mustache.js template
     emptyMsg  : 'Nothing found.'    // shown message if search returns no results
   };
