3734afb4f56df5729647ef03af28fee1b656f2d6
Ben Smith <ben@10consulting.com>
2016-08-23 19:34:41 +0100

    Build and bump version to 3.2.0

--- build/jekyll_lunr_js_search.rb
+++ build/jekyll_lunr_js_search.rb
@@ -256,6 +256,6 @@ module Jekyll
 end
 module Jekyll
   module LunrJsSearch
-    VERSION = "3.1.0"
+    VERSION = "3.2.0"
   end
 end
--- lib/jekyll_lunr_js_search/version.rb
+++ lib/jekyll_lunr_js_search/version.rb
@@ -1,5 +1,5 @@
 module Jekyll
   module LunrJsSearch
-    VERSION = "3.1.0"
+    VERSION = "3.2.0"
   end
 end
